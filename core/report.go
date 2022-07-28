807cf9e6418497af2f2da3dfd651d19d734fa74a
kitsch <dream_hollow@foxmail.com>
2022-07-29 06:00:25 +0800

    Xlsx报告输出部分完成

--- core/report.go
+++ core/report.go
@@ -5,15 +5,50 @@ import "github.com/xuri/excelize/v2"
 func XlsxInit(output string) error {
 	file := excelize.NewFile()
 
+	//初始化ip表
 	//创建表
 	IpIndex := file.NewSheet("IP地址")
 	//初始数据
-	categories := map[string]string{"A1": "IP地址", "B1": "City", "C1": "Country", "D1": "Cidr"}
-	for k, v := range categories {
-		file.SetCellValue("ip地址", k, v)
+	IpCategories := map[string]string{"A1": "IP", "B1": "City", "C1": "Country"}
+	for k, v := range IpCategories {
+		file.SetCellValue("IP地址", k, v)
 	}
-	file.DeleteSheet("Sheet1")
+
+	//初始化cidr表
+	CidrIndex := file.NewSheet("C段信息")
+	CidrCategories := map[string]string{"A1": "Cidr", "B1": "City", "C1": "Country", "D1": "Domain", "E1": "IP"}
+	for k, v := range CidrCategories {
+		file.SetCellValue("C段信息", k, v)
+	}
+	file.SetActiveSheet(CidrIndex)
+
+	//初始化端口服务表
+	PortIndex := file.NewSheet("端口服务")
+	PortCategories := map[string]string{"A1": "IP", "B1": "Port", "C1": "Server", "D1": "Banner"}
+	for k, v := range PortCategories {
+		file.SetCellValue("端口服务", k, v)
+	}
+	file.SetActiveSheet(PortIndex)
+
+	//初始化域名指纹表
+	DomainFingerIndex := file.NewSheet("域名指纹")
+	DomainFingerCategories := map[string]string{"A1": "Url", "B1": "IsCDN", "C1": "StatusCode", "D1": "Header", "E1": "Length", "F1": "Title", "G1": "Finger", "H1": "IsHoneypot"}
+	for k, v := range DomainFingerCategories {
+		file.SetCellValue("域名指纹", k, v)
+	}
+	file.SetActiveSheet(DomainFingerIndex)
+
+	//初始化IP指纹表
+	IpFingerIndex := file.NewSheet("IP指纹")
+	IpFingerCategories := map[string]string{"A1": "Url", "B1": "StatusCode", "C1": "Header", "D1": "Length", "E1": "Title", "F1": "Finger", "G1": "IsHoneypot"}
+	for k, v := range IpFingerCategories {
+		file.SetCellValue("IP指纹", k, v)
+	}
+	file.SetActiveSheet(IpFingerIndex)
+
 	file.SetActiveSheet(IpIndex)
+
+	file.DeleteSheet("Sheet1")
 	err := file.SaveAs(output)
 	if err != nil {
 		return err
