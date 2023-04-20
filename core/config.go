2174f3519cf2163b5d4e405114ba345a02ac205a
kitsch <dream_hollow@foxmail.com>
2023-04-20 12:51:05 +0800

    update

--- core/config.go
+++ core/config.go
@@ -3,4 +3,5 @@ package core
 const OutDir = "./out"
 const Version = "1.0"
 const QqwryPath = "/config/qqwry.dat"
-const FingerPrintPath = "/config/web/web_fingerprint_v3.json"
+
+//const FingerPrintPath = "/config/web/web_fingerprint_v3.json"
